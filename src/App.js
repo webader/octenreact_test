c744a352e4b86120228167eddda31e85eccc3f0f
webader <alexey.derenko@gmail.com>
2023-05-04 18:22:39 +0300

    lesson 3 form part 2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import React, {useState} from 'react';
+import React, {createRef} from 'react';
 
 const App = () => {
-    // let [login, setLogin] = useState('Put your login');
-    // let [password, setPassword] = useState('Put your password');
-    // //для того щоб форма стала контрольованою привязуємо useState
-    //
-    // function onLoginChange(e) {
-    //     setLogin(e.target.value);
-    // }
-    // function onPasswordChange(e) {
-    //     setPassword(e.target.value);
-    // }
+    // це називається не контрольований компонент тому що реакт не контролює стан наших інпутів
+    let login = createRef(); // reference object
+    let password = createRef()
 
-    //2 варіант
-    let [formState, setFormState] = useState({login: '', password: ''})
     function onSubmit(e) {
-        console.log(e.target.login.value);
-        console.log(e.target.password.value);
+        console.log(login.current.value);
+        console.log(password.current.value);
+        //create obj && send to api
         e.preventDefault();
-        console.log(formState);
-    }
-
-    function onChange(e) {
-        console.log({[e.target.name]:e.target.value});
-        setFormState({...formState, [e.target.name]:e.target.value})
     }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" name={'login'} value={formState.login} onChange={onChange}/>
-                <input type="text" name={'password'} value={formState.password} onChange={onChange}/>
-                <button>submit</button>
+                <input type="text" name={'login'} ref={login}/>
+                <input type="text" name={'password'} ref={password}/>
+                <button>log in</button>
             </form>
         </div>
     );
