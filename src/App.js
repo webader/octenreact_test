8e4642d8ceda9a99c73c396ae65536b4e4fb2c77
webader <alexey.derenko@gmail.com>
2023-05-05 09:19:32 +0300

    lesson 3 form part 3 useForm

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import React, {useState} from 'react';
+import {useForm} from "react-hook-form";
 
 const App = () => {
-    // let [login, setLogin] = useState('Put your login');
-    // let [password, setPassword] = useState('Put your password');
-    // //для того щоб форма стала контрольованою привязуємо useState
-    //
-    // function onLoginChange(e) {
-    //     setLogin(e.target.value);
-    // }
-    // function onPasswordChange(e) {
-    //     setPassword(e.target.value);
-    // }
-
-    //2 варіант
-    let [formState, setFormState] = useState({login: '', password: ''})
-    function onSubmit(e) {
-        console.log(e.target.login.value);
-        console.log(e.target.password.value);
-        e.preventDefault();
-        console.log(formState);
+    let {register,handleSubmit} = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+        //...catch obj
     }
 
-    function onChange(e) {
-        console.log({[e.target.name]:e.target.value});
-        setFormState({...formState, [e.target.name]:e.target.value})
-    }
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input type="text" name={'login'} value={formState.login} onChange={onChange}/>
-                <input type="text" name={'password'} value={formState.password} onChange={onChange}/>
-                <button>submit</button>
+
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("title", {required:true})}/>  {/*// тут register створює автоматично інпут з наймом title*/}
+                <input {...register("body")}/>
+                <input type='submit' value={'save post'}/>
             </form>
         </div>
     );
